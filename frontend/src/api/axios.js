81e848054ccd3156038f180ad897a3596712ab5f
Raunak Rai <raunak96963@gmail.com>
2026-04-12 19:25:47 +0530

    fix axios auth properly

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://finflow-backend-qt03.onrender.com',
+  baseURL: 'https://finflow-backend-qt03.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
